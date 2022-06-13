3ac6c292b0e8651907cfc7ca5577278916d0be64
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-06-13 16:30:48 +0300

    ВЫПОЛНЕНО

---
+++ index.js
@@ -0,0 +1,8 @@
+// Простое использование:
+
+var  figlet  =  require ( 'figlet' ) ;
+
+figlet ( 'intocode' ,  function ( err ,  data )  { 
+    if  ( err )  { 
+        console.log ( ' Что - то пошло не так ... ' ) ; console.dir ( err ) ; return ; } console.log ( data ) } ) ;
+
